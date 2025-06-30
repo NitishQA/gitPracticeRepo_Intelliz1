ce3ee1eefb98cc8fe49f52971f65862e17b48251
NitishQA <nitishkumarqa@gmail.com>
2025-06-30 20:24:54 +0530

    Commit 2 From Home for [Edit 3 from Home]

--- src/main/java/MainClass1.java
+++ src/main/java/MainClass1.java
@@ -1,4 +1,6 @@
-public class MainClass1 {
+public class MainClass1
+{
 //    Edit 1 Main class from home1
 //    Edit 2 from Office1
+//    Edit 3 from Home
 }
--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -2,4 +2,5 @@ public class TestClass1
 {
 //    Edit 1 from home
 //    Edit 2 from Office1
+//    Edit 3 from Home
 }
